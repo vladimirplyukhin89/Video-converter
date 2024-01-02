2bbe6fafe86c2564dc8191052f0cff9f7ace1126
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2024-01-03 00:10:49 +0500

    Начало

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+const one = 1;
+function getFullName(firstName, secondName) {
+    return `${firstName} ${secondName}`;
+}
+console.log(getFullName('Anastsia', 'Plyukhina'));
---
+++ app.ts
@@ -0,0 +1,6 @@
+const one = 1;
+function getFullName(firstName: string, secondName: string): string {
+  return `${firstName} ${secondName}`
+}
+
+console.log(getFullName('Anastasia', 'Plyukhina'));
